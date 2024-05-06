9451b0245732b5b273701c4cee723748aaa718d4
AdiRaushan <adiraushan2k@gmail.com>
2024-05-06 23:48:02 +0530

    useEffect

--- currency-covertor-react/src/hooks/useCurrencyInfo.js
+++ currency-covertor-react/src/hooks/useCurrencyInfo.js
@@ -3,7 +3,9 @@ import { useEffect, useState } from "react";
 /* Side effects are things your component does besides rendering, like fetching data, subscribing to events, or updating the DOM directly. multi well*/
 
 const useCurrencyInfo = (currency) => {
-  return <div></div>;
+  useEffect(() => {
+    fetch();
+  }, []);
 };
 
 export default useCurrencyInfo;
