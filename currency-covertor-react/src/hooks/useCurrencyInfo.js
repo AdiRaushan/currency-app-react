2f0286f91a9ec0647ced8be416da1be3c0c54ba5
AdiRaushan <adiraushan2k@gmail.com>
2024-05-07 09:58:56 +0530

    update

--- currency-covertor-react/src/hooks/useCurrencyInfo.js
+++ currency-covertor-react/src/hooks/useCurrencyInfo.js
@@ -3,9 +3,17 @@ import { useEffect, useState } from "react";
 /* Side effects are things your component does besides rendering, like fetching data, subscribing to events, or updating the DOM directly. multi well*/
 
 const useCurrencyInfo = (currency) => {
+  const [data, setData] = useState({});
+
   useEffect(() => {
-    fetch();
-  }, []);
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  console.log(data);
+  return data;
 };
 
 export default useCurrencyInfo;
