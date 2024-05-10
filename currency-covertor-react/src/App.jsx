ddc105aa8cae74c0b3d27e2529bfed5cfa1a4b24
AdiRaushan <adiraushan2k@gmail.com>
2024-05-10 23:54:41 +0530

    to the point

--- currency-covertor-react/src/App.jsx
+++ currency-covertor-react/src/App.jsx
@@ -1,27 +1,65 @@
 import Form from "./Form";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
+import { InputBox } from "./components/InputBox";
+import { useState } from "react";
 
 const App = () => {
-  const firstName = Form("");
-  const lastName = Form("");
+  const [amount, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [covertedAmount, SetcovertedAmount] = useState("");
 
-  console.log(useCurrencyInfo);
+  const currencyInfo = useCurrencyInfo(from);
+
+  const options = Object.keys(currencyInfo);
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    SetcovertedAmount(amount);
+    setAmount(covertedAmount);
+  };
+  const convert = () => {
+    SetcovertedAmount(amount * currencyInfo[to]);
+  };
   return (
-    <form>
-      <label>
-        First Name:
-        <input type="text" {...firstName} />
-      </label>
-      <br />
-      <label className="py-4">
-        Last Name:
-        <input type="text" {...lastName} />
-      </label>
-      <br />
-      <p>
-        Full Name: {firstName.value} {lastName.value}
-      </p>
-    </form>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('${BackgroundImage}')`,
+      }}
+    >
+      //hello
+      <div className="w-full">
+        <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="w-full mb-1">
+              <InputBox label="From" />
+            </div>
+            <div className="relative w-full h-0.5">
+              <button
+                type="button"
+                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+              >
+                swap
+              </button>
+            </div>
+            <div className="w-full mt-1 mb-4">
+              <InputBox label="To" />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+            >
+              Convert
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- currency-covertor-react/src/components/InputBox.jsx
+++ currency-covertor-react/src/components/InputBox.jsx
@@ -1,3 +1,5 @@
+import { useId } from "react";
+
 const InputBox = ({
   label,
   amount,
@@ -9,11 +11,15 @@ const InputBox = ({
   currencyDiable = false,
   className = "",
 }) => {
+  const amountId = useId();
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className} `}>
       <div className="w-1/2">
-        <label className="text-black/40 mb-2 inline-block">{label}</label>
+        <label id={amountId} className="text-black/40 mb-2 inline-block">
+          {label}
+        </label>
         <input
+          id={amountId}
           className="outline-none w-full bg-transparent py-1.5"
           type="number"
           placeholder="Amount"
